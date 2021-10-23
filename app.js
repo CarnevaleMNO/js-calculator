a8b129f314f2efa266f504f4be2032150379d9fc
Matthew Carnevale <carnevalemno@gmail.com>
2021-10-23 18:34:09 +0900

    Version 1.0

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const nums = document.querySelectorAll(".num");
 const ops = document.querySelectorAll(".op");
 const equals = document.querySelector(".equals");
 const clear = document.querySelector(".clear");
-const point = document.querySelector('.point');
-const del = document.querySelector('.del');
+const point = document.querySelector(".point");
+const del = document.querySelector(".del");
+const zero = document.querySelector(".zero");
 
 //Variables
 display.innerHTML = "";
@@ -24,10 +25,16 @@ clear.addEventListener("click", function () {
   topDisplay.innerHTML = "";
   operator = "";
 });
-del.addEventListener('click', function(){
+del.addEventListener("click", function () {
   display.innerHTML = display.innerHTML.slice(0, -1);
-  
-})
+});
+zero.addEventListener("click", function () {
+  if (display.innerHTML === "0") {
+    return (display.innerHTML = display.innerHTML);
+  } else {
+    display.innerHTML = display.innerHTML + zero.value;
+  }
+});
 
 //Form Control
 form.addEventListener("submit", function (eventObject) {
@@ -53,30 +60,45 @@ const calculate = (num1, num2, op) => {
 };
 equals.addEventListener("click", function () {
   let total;
-  number2 = Number(display.innerHTML);
-  total = calculate(number1, number2, operator);
-  topDisplay.innerHTML = total;
-  display.innerHTML = "";
-  operator = "";
-  number1 = 0;
-  number2 = 0;
+  if (operator) {
+    number2 = Number(display.innerHTML);
+    if (number2 !== "0") {
+      total = calculate(number1, number2, operator);
+      topDisplay.innerHTML = total;
+      display.innerHTML = "";
+      operator = "";
+      number1 = 0;
+      number2 = 0;
+    } else {
+      display.innerHTML = "Infinity";
+    }
+  }
 });
 
 //Calc Keys
 for (let num of nums) {
   num.addEventListener("click", function () {
-    if (!operator && topDisplay.innerHTML) {
-      number1 = 0;
-      number2 = 0;
-      topDisplay.innerHTML = "";
-      operator = "";
-      return (display.innerHTML = display.innerHTML + num.value);
+    if (display.innerHTML.length < 15) {
+      if (!operator && topDisplay.innerHTML) {
+        number1 = 0;
+        number2 = 0;
+        topDisplay.innerHTML = "";
+        operator = "";
+        return (display.innerHTML = display.innerHTML + num.value);
+      }
+      display.innerHTML = display.innerHTML + num.value;
     }
-    display.innerHTML = display.innerHTML + num.value;
+    display.innerHTML = display.innerHTML;
   });
 }
 for (let op of ops) {
   op.addEventListener("click", function () {
+    if (
+      topDisplay.innerHTML === "Please input a number or an operator" ||
+      topDisplay.innerHTML === "Infinity"
+    ) {
+      return (topDisplay.innerHTML = "Please input a number or an operator");
+    }
     if (!topDisplay.innerHTML && !display.innerHTML) {
       operator = operator;
       topDisplay.innerHTML = topDisplay.innerHTML;
@@ -86,27 +108,27 @@ for (let op of ops) {
     } else if (!topDisplay.innerHTML) {
       operator = op.value;
       number1 = Number(display.innerHTML);
-      topDisplay.innerHTML = display.innerHTML + ' ' + operator + ' ';
+      topDisplay.innerHTML = display.innerHTML + " " + operator + " ";
       display.innerHTML = "";
     } else if (number1 && operator && display.innerHTML) {
       let total;
       number2 = Number(display.innerHTML);
       total = calculate(number1, number2, operator);
       operator = op.value;
-      topDisplay.innerHTML = total + ' ' + operator + ' ';
+      topDisplay.innerHTML = total + " " + operator + " ";
       number1 = total;
       number2 = 0;
       display.innerHTML = "";
     } else if (topDisplay.innerHTML) {
       operator = op.value;
       number1 = Number(topDisplay.innerHTML);
-      topDisplay.innerHTML = topDisplay.innerHTML + ' ' + operator + ' ';
+      topDisplay.innerHTML = topDisplay.innerHTML + " " + operator + " ";
     }
   });
 }
-point.addEventListener('click', function(){
-  if(!display.innerHTML.includes(point.value)){
+point.addEventListener("click", function () {
+  if (!display.innerHTML.includes(point.value)) {
     display.innerHTML = display.innerHTML + point.value;
   }
   display.innerHTML = display.innerHTML;
-})
+});
